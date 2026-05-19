cb37c08176aa93e823d07627290ca890f50897b7
nava <nava@noreply.com>
2026-05-18 23:54:11 -0500

    simple!

--- build/intermediates/cxx/Debug/225s6l69/logs/arm64-v8a/build_command_matrix_audio.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\PPProgam\\android_sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\incxiuefb\\Documents\\Files\\clone\\audio_recorder\\audioengine\\.cxx\\Debug\\225s6l69\\arm64-v8a" ^
-  matrix_audio
--- build/intermediates/cxx/RelWithDebInfo/52194mo6/logs/armeabi-v7a/build_command_matrix_audio.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\PPProgam\\android_sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\incxiuefb\\Documents\\Files\\clone\\audio_recorder\\audioengine\\.cxx\\RelWithDebInfo\\52194mo6\\armeabi-v7a" ^
-  matrix_audio
